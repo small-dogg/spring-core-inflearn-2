8f1b27ba1492d8d01ca04e5124c573977c792a44
small-dogg <sy920112@naver.com>
2022-04-12 03:27:02 +0900

    feat: ApplcationContext를 통해 만든 빈을 호출하는 예제

--- src/main/java/com/smalldogg/springcoreinflearn/AppConfig.java
+++ src/main/java/com/smalldogg/springcoreinflearn/AppConfig.java
@@ -1,7 +1,6 @@
 package com.smalldogg.springcoreinflearn;
 
 import com.smalldogg.springcoreinflearn.discount.DiscountPolicy;
-import com.smalldogg.springcoreinflearn.discount.FixDiscountPolicy;
 import com.smalldogg.springcoreinflearn.discount.RateDiscountPolicy;
 import com.smalldogg.springcoreinflearn.member.MemberRepository;
 import com.smalldogg.springcoreinflearn.member.MemberService;
@@ -9,23 +8,29 @@ import com.smalldogg.springcoreinflearn.member.MemberServiceImpl;
 import com.smalldogg.springcoreinflearn.member.MemoryMemberRepository;
 import com.smalldogg.springcoreinflearn.order.OrderService;
 import com.smalldogg.springcoreinflearn.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-  //생성자 주입
+  @Bean
   public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
   }
 
-  private MemberRepository memberRepository() {
+  @Bean
+  public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  public DiscountPolicy discountPolicy(){
+  @Bean
+  public DiscountPolicy discountPolicy() {
     return new RateDiscountPolicy();
   }
 
